bc9476d578226ac49941279b7c2a1b8eb5ad15c8
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-25 01:17:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,14 +21,14 @@ class InteractiveRecord
     column_names.compact
   end
 
-    
+
   def initialize(option={})
 
     self.column_names.each do |col_name|
       attr_accessor col_name.to_sym
     end
 
-    
+
     option.each do |property, value|
       self.send("#{property}=", value)
     end
