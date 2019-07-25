bb9625d90b75f202ad36f23e45a42c4db6e378ef
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-25 01:02:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,9 +21,8 @@ class InteractiveRecord
     column_names.compact
   end
 
-  def self.column_names.each do |col_name|
+  self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
-    end
   end
 
 
